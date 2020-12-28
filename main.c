dc4a4827b84088f7742885ab688f4d83229f812b
Charmander <~@charmander.me>
2020-12-28 00:36:47 -0800

    Read mozLz4 header
    to be able to run directly on a file.

--- main.c
+++ main.c
@@ -4,10 +4,18 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <lz4.h>
 
 #define BUF_CAPACITY_INITIAL ((size_t)4096)
 #define READ_SIZE ((size_t)4096)
+
+static uint8_t MOZLZ4_HEADER[8] = "mozLz40";
+
+enum {
+	MOZLZ4_SIZE_HEADER_SIZE = 4,
+	MOZLZ4_LZ4_OFFSET = sizeof MOZLZ4_HEADER + MOZLZ4_SIZE_HEADER_SIZE,
+};
 
 struct buf {
 	uint8_t* contents;
@@ -93,8 +101,13 @@ int main(void) {
 		goto free_fail;
 	}
 
-	if (buf.length < 4) {
+	if (buf.length < MOZLZ4_LZ4_OFFSET) {
 		fprintf(stderr, "Input of %zu bytes is too small to contain size header\n", buf.length);
+		goto free_fail;
+	}
+
+	if (memcmp(buf.contents, MOZLZ4_HEADER, sizeof MOZLZ4_HEADER) != 0) {
+		fputs("Missing or invalid mozLz4 header\n", stderr);
 		goto free_fail;
 	}
 
@@ -102,10 +115,10 @@ int main(void) {
 
 	{
 		uint_least32_t const header_size_u =
-			  (uint_least32_t)buf.contents[0]
-			| (uint_least32_t)buf.contents[1] << 8
-			| (uint_least32_t)buf.contents[2] << 16
-			| (uint_least32_t)buf.contents[3] << 24;
+			  (uint_least32_t)buf.contents[sizeof MOZLZ4_HEADER + 0]
+			| (uint_least32_t)buf.contents[sizeof MOZLZ4_HEADER + 1] << 8
+			| (uint_least32_t)buf.contents[sizeof MOZLZ4_HEADER + 2] << 16
+			| (uint_least32_t)buf.contents[sizeof MOZLZ4_HEADER + 3] << 24;
 
 		if (header_size_u > (uint_least32_t)INT_MAX) {
 			fprintf(stderr, "Declared decompressed size of %" PRIuLEAST32 " bytes is too large\n", header_size_u);
@@ -123,15 +136,15 @@ int main(void) {
 		return EXIT_FAILURE;
 	}
 
-	if (buf.length - 4 > INT_MAX) {
+	if (buf.length - MOZLZ4_LZ4_OFFSET > INT_MAX) {
 		fprintf(stderr, "Input of %zu bytes is too large\n", buf.length);
 		goto free_fail;
 	}
 
 	int const decompressed_count = LZ4_decompress_safe(
-		(char const*)&buf.contents[4],
+		(char const*)&buf.contents[MOZLZ4_LZ4_OFFSET],
 		(char*)decompressed,
-		(int)(buf.length - 4),
+		(int)(buf.length - MOZLZ4_LZ4_OFFSET),
 		header_size
 	);
 
